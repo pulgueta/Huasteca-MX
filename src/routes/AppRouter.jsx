fb24ea7c83a16441cc962ff229c1d3bc66f8c0a6
Andrés Rodríguez <retardix456@gmail.com>
2022-08-01 22:36:08 -0500

    not used rm

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { Footer, Navigation } from "../components";
+import { Footer } from "../components";
 import { Navbar } from "../components/Navbar";
 import { Landing, SignUp, SignIn, Article, ArticleDetails, CulturalTour, CityReport, CulturalTourDetails, About, ForgotPassword } from "../views";
 
