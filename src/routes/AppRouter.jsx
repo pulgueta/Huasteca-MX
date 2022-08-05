371be356cf6b0070f38a8e099e6fc0b3dccc9327
Andrés Rodríguez <retardix456@gmail.com>
2022-08-04 23:37:47 -0500

    app router

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { Footer, Layout, Navbar } from "../components";
+import { Footer, Navbar } from "../components";
 import {
   Landing,
   SignUp,
