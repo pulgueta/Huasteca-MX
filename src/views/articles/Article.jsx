75dc79b8661053ff527e1d6b0a6047853ccf4bd6
Andrés Rodríguez <retardix456@gmail.com>
2022-09-07 19:43:20 -0500

    fix

--- src/views/articles/Article.jsx
+++ src/views/articles/Article.jsx
@@ -3,28 +3,36 @@ import { useState, useEffect } from "react";
 import { queryData } from "../../utils/firebase";
 
 export const Article = () => {
-  const [dataArticles, setDataArticles] = useState([])
+  const [dataArticles, setDataArticles] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const docs = await queryData('articles')
-      const data = docs
-      const array = []
-      data.forEach(element => {
-        if (element.data().state === 'Activo') {
+      const docs = await queryData("articles");
+      const data = docs;
+      const array = [];
+      data.forEach((element) => {
+        if (element.data().state === "Activo") {
           array.push({
             id: element.id,
-            ...element.data()
-          })
+            ...element.data(),
+          });
         }
       });
-      setDataArticles(array)
-    }
-    getData()
-  }, [])
+      setDataArticles(array);
+    };
+    getData();
+  }, []);
 
   return (
-    <div className={`${dataArticles.length < 2 ? 'h-screen' : 'h-full'} py-10 px-5 bg-neutral-400 flex flex-col items-center md:px-10 md:${dataArticles.length < 2 ? 'h-screen' : 'h-full'} md:py-16 lg:${dataArticles.length < 2 ? 'h-screen' : 'h-full'} lg:grid lg:grid-cols-2 lg:gap-10`}>
+    <div
+      className={`${
+        dataArticles.length < 2 ? "h-screen" : "h-screen"
+      } py-10 px-5 bg-neutral-400 flex flex-col items-center md:px-10 md:${
+        dataArticles.length < 2 ? "h-screen" : "h-screen"
+      } md:py-16 lg:${
+        dataArticles.length < 2 ? "h-screen" : "h-screen"
+      } lg:grid lg:grid-cols-2 lg:gap-10`}
+    >
       {dataArticles.length > 0 &&
         dataArticles.map(({ id, mainImage, title, description, date }) => {
           return (
